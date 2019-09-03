7a8ddab2e99e1482a7b8b2db1410291cfa210f2c
Fei Ding <feiding@tamu.edu>
2019-09-03 00:34:43 -0500

    Complete SimpleProgram.R document -- Generate simple data to test the functions in FunctionsLM.R

--- SimpleProgram.R
+++ SimpleProgram.R
@@ -8,8 +8,14 @@ sigma = 2 # noise standard deviation
 beta = rep(2,p) # true vector of coefficients
 X = matrix(rnorm(n*p), n, p) # n by p matrix of predictors
 
+# source the functions we write in document -- FunctionsLM.R
+source('./FunctionsLM.R')
+
 # [ToDo] Use generateY function to generate Y
+Y <- generateY(X = X, beta = beta,sigma = sigma)
 
 # [ToDo] Use calculateBeta function to calculate beta_LS
+beta_LS <- calculateBeta(X = X,Y = Y)
 
 # [ToDo] Use calculateMSE to assess the estimation error measured by squared eucledian distance - ||beta - beta_LS||_2^2
+Error <- calculateMSE(beta = beta,beta_LS = beta_LS)
