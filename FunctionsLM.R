f6ec9416d0993776b82c83972503a20f69d57424
Fei Ding <feiding@tamu.edu>
2019-09-02 23:30:32 -0500

    write the generateY function

--- FunctionsLM.R
+++ FunctionsLM.R
@@ -5,7 +5,9 @@
 # seed  - starting seed value
 generateY <- function(X, beta, sigma, seed = 5832652){
   #[ToDo] Set seed and generate Y following linear model
-  
+  nobs <- dim(X)[1] # get the number of observations
+  noise <- rnorm(n = nobs, mean = 0, sd = sigma) # generate the noise term
+  Y <- X %*% beta + noise # generate the observations
   # Return Y
   return(Y)
 }
